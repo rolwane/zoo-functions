e6dc93ff52c412fd01bec2e3ae0c0520e44f6fa5
Rolwane Borges <rolwane@hotmail.com>
2021-11-24 15:20:55 -0300

    Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Encontra a espécie referente ao nome passado como parâmetro.
+  const specie = data.species.find((spec) => spec.name === animal);
+
+  // Verifica se os residentes da espécie encontrada possuem a idade mínima passada.
+  return specie.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
