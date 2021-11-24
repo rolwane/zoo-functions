05c5382d465d58de24fdc99c13ff68204ec9e9e0
Rolwane Borges <rolwane@hotmail.com>
2021-11-24 17:21:15 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(species) {
+  if (species === undefined) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  const findAnimals = data.species.find((specie) => specie.name === species.specie).residents;
+  if (species.sex === undefined) return findAnimals.length;
+  return findAnimals.reduce((acc, animal) => {
+    const paramSex = species.sex;
+    return animal.sex === paramSex ? acc + 1 : acc;
+  }, 0);
 }
 
 module.exports = countAnimals;
