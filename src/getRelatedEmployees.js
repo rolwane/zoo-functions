5d2895c96d759afa472b74b73197a3d51ca12964
Rolwane Borges <rolwane@hotmail.com>
2021-11-24 16:45:07 -0300

    Requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.some((manegerId) => id === manegerId));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const employees = data.employees.filter((employee) => {
+      const employeeManagers = employee.managers;
+      return employeeManagers.some((managerIds) => managerIds === managerId);
+    });
+    return employees.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,4 +5,6 @@ function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 
+console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
+
 module.exports = getSpeciesByIds;
