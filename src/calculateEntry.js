353ec01b82af2b92f3bc938195512c6a6e56d9b5
Rolwane Borges <rolwane@hotmail.com>
2021-11-24 18:12:35 -0300

    Requisito 06

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, client) => {
+    if (client.age < 18) {
+      acc.child += 1;
+    } else if (client.age >= 18 && client.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const quantidade = countEntrants(entrants);
+  const { adult, senior, child } = data.prices;
+  return (quantidade.adult * adult) + (quantidade.senior * senior) + (quantidade.child * child);
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -16,6 +16,4 @@ function getRelatedEmployees(managerId) {
   }
 }
 
-console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
-
 module.exports = { isManager, getRelatedEmployees };
