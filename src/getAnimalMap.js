8eea21947ed88c845686cc13e4591a1a9a8c8105
Rolwane Borges <rolwane@hotmail.com>
2021-11-25 16:59:17 -0300

    depois de 6 horas tentando... Requisito 07!

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
+function categorizedByLocation({ species }) {
+  const objBase = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((acc, specie) => {
+    acc[specie.location].push(specie.name);
+    return acc;
+  }, objBase);
+}
 
+function categorizedWithNames({ species }, { sorted }) {
+  const objBase = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((objeto, specie) => {
+    const objetoInterno = {};
+    objetoInterno[specie.name] = specie.residents.map((animal) => animal.name);
+    if (sorted) objetoInterno[specie.name].sort();
+    objeto[specie.location].push(objetoInterno);
+    return objeto;
+  }, objBase);
+}
+
+function categorizedBySex({ species }, { sex, sorted }) {
+  const objBase = { NE: [], NW: [], SE: [], SW: [] };
+  return species.reduce((objeto, specie) => {
+    const objetoInterno = {};
+    objetoInterno[specie.name] = [];
+    specie.residents.forEach((animal) => {
+      if (animal.sex === sex) objetoInterno[specie.name].push(animal.name);
+    });
+    if (sorted) objetoInterno[specie.name].sort();
+    objeto[specie.location].push(objetoInterno);
+    return objeto;
+  }, objBase);
+}
+
+function getAnimalMap(options) {
+  if (options === undefined || options.includeNames !== true) return categorizedByLocation(data);
+  if (options.sex) return categorizedBySex(data, options);
+  return categorizedWithNames(data, options);
 }
 
 module.exports = getAnimalMap;
