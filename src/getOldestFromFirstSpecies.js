50cf4e0367525bbb911a4b5735ca5d5062909b02
Rolwane Borges <rolwane@hotmail.com>
2021-11-26 09:03:48 -0300

    Requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { species, employees } = data;
+
+  // Encontro o funcionário referente a ID passada
+  const employeeFound = employees.find((employee) => employee.id === id);
+
+  // Encontro a primeira espécie que o funcionário gerencia
+  const specieFound = species.find((specie) => specie.id === employeeFound.responsibleFor[0]);
+
+  // Encontro o animal mais velho
+  const olderAnimal = specieFound.residents.reduce((acc, animal) => {
+    if (acc.age > animal.age) return acc;
+    return animal;
+  });
+
+  return ([`${olderAnimal.name}`, `${olderAnimal.sex}`, olderAnimal.age]);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
